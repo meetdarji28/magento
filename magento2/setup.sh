94bbf643676af6b85da5492a23d4f1f87a72cc40
NLS_Mitkumar <mit.parmar@nexuslinkservices.in>
2025-05-27 16:04:00 +0530

    New code uploaded

---
+++ magento2/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -a
+source .env
+set +a
+
+cd /var/www/html/magento2
+
+php -d memory_limit=1025M bin/magento setup:install \
+  --base-url=http://localhost:8300 \
+  --db-host=$MYSQL_HOST \
+  --db-name=$MYSQL_DATABASE \
+  --db-user=$MYSQL_USER \
+  --db-password=$MYSQL_PASSWORD \
+  --backend-frontname=admin \
+  --admin-firstname=Admin \
+  --admin-lastname=User \
+  --admin-email=$ADMIN_EMAIL \
+  --admin-user=$ADMIN_USER \
+  --admin-password=$ADMIN_PASS \
+  --language=en_US \
+  --currency=USD \
+  --timezone=UTC \
+  --use-rewrites=1 \
+  --search-engine=elasticsearch7 \
+  --elasticsearch-host=$ES_HOST \
+  --elasticsearch-port=$ES_PORT \
+  --session-save=redis \
+  --session-save-redis-host=$REDIS_HOST \
+  --session-save-redis-port=$REDIS_PORT \
+  --session-save-redis-password=$REDIS_PASSWORD
